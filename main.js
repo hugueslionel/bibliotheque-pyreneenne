b622588f014eefcba4e667cf27086006e79d283a
hugueslionel <hugueslionel@hotmail.com>
2025-05-21 11:13:08 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
