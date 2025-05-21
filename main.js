682aa4618bfed766c8ae0fc2df2e5433f7965894
hugueslionel <hugueslionel@hotmail.com>
2025-05-21 11:15:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const fs = require('fs');
 const path = require('path');
 
 let mainWindow;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      enableRemoteModule: false,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  });
+ipcMain.handle('export-data', async (event, data) => {
+    const { filePath } = await dialog.showSaveDialog({
+        title: 'Exporter les données',
+        defaultPath: path.join(app.getPath('documents'), 'bibliotheque_pyreneenne.json'),
+        filters: [{ name: 'JSON Files', extensions: ['json'] }]
+    });
 
-  mainWindow.loadFile('index.html');
+    if (filePath) {
+        await fs.promises.writeFile(filePath, data);
+        return { filePath };
+    }
+    return {};
+});
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    mainWindow.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
